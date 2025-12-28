4681c26a4b536bd246563e0df02f23cdd7724b87
Jason Sweet <hoozifachi@gmail.com>
2025-12-28 16:22:05 -0600

    Adds event demo and crud updates.

---
+++ crud-app2/src/server.tsx
@@ -0,0 +1,160 @@
+import { type Context, Hono } from 'hono';
+import { serveStatic } from 'hono/bun';
+
+const app = new Hono();
+
+// Serve static files from the public directory
+app.use('/*', serveStatic({ root: './public' }));
+
+type Dog = { id: string, name: string, breed: string };
+
+const dogs = new Map<string, Dog>();
+
+function addDog(name: string, breed: string): Dog {
+  const id = crypto.randomUUID();
+  const dog = { id, name, breed };
+  dogs.set(id, dog);
+  return dog;
+}
+
+addDog('Comet', 'Whippet');
+addDog('Oscar', 'German Shorthaired Pointer');
+
+let selectedId = '';    // Holds the id of the currently selected dog
+
+function dogRow(dog: Dog, updating = false) {
+  // If the dog is being updated, we want to perfornm an out-of-band swap
+  // SO a new table row can be replaced the existing one.
+  const attrs: { [key: string]: string } = {};
+  if (updating) attrs['hx-swap-oob'] = 'true';
+
+  return (
+    <tr class="on-hover" id={`row-${dog.id}`} {...attrs}>
+      <td>{dog.name}</td>
+      <td>{dog.breed}</td>
+      <td class="buttons">
+        {/* Clicking this button askes the user if they really want 
+            to delete the dog and does so if confirmed. */}
+        <button
+          class="show-on-hover"
+          hx-confirm="Are you sure?"
+          hx-delete={`/dog/${dog.id}`}
+          hx-target="closest tr"
+          hx-swap="outerHTML"
+          type="button"
+        >
+          ✕
+        </button>
+        {/* Clicking this button selects the dog which triggers a
+            selection-change event. That causes the form to update
+            so the user can modify the name and/or breed of the dog. */}
+        <button
+          class="show-on-hover"
+          hx-put={'/select/' + dog.id}
+          hx-swap="none"
+          type="button"
+        >
+          Edit
+        </button>
+      </td>
+    </tr>
+  );
+}
+
+app.get('/form', (c: Context) => {
+  const attrs: { [key: string]: string } = {
+    'hx-on:htmx:after-request': 'this.reset()'
+  };
+
+  if (selectedId) {
+    // Update exisitng row
+    attrs['hx-put'] = '/dog/' + selectedId;
+  } else {
+    // Add a new row.
+    attrs['hx-post'] = '/dog';
+    attrs['hx-target'] = 'tbody';
+    attrs['hx-swap'] = 'afterbegin';
+  }
+  const selectedDog = dogs.get(selectedId);
+
+  return c.html(
+    <form hx-disabled-elt="#submit-btn" {...attrs}>
+      <div>
+        <label for="name">Name</label>
+        <input
+          id="name"
+          name="name"
+          required
+          size={30}
+          type="text"
+          value={selectedDog?.name ?? ''}
+        />
+      </div>
+      <div>
+        <label htmlFor="breed">Breed</label>
+        <input
+          id='breed'
+          name='breed'
+          required
+          size={30}
+          type="text"
+          value={selectedDog?.breed ?? ''}
+        />
+      </div>
+      <div class="buttons">
+        <button id="submit-btn">{selectedId ? 'Update' : 'Add'}</button>
+        {selectedId && (
+          <button hx-put="/deselect" hx-swap="none" type='button'>
+            Cancel
+          </button>
+        )}
+      </div>
+    </form>
+  )
+});
+
+app.get('/table-rows', (c: Context) => {
+  const sortedDogs = Array.from(dogs.values()).sort((a, b) => a.name.localeCompare(b.name));
+  return c.html(<>{sortedDogs.map(dog => dogRow(dog))}</>)
+});
+
+app.post('/dog', async (c: Context) => {
+  const formData = await c.req.formData();
+  const name = (formData.get('name') as string) || '';
+  const breed = (formData.get('breed') as string) || '';
+  const dog = addDog(name, breed);
+  return c.html(dogRow(dog), 201);
+});
+
+app.put('select/:id', (c: Context) => {
+  selectedId = c.req.param('id');
+  c.header('HX-Trigger', 'selection-change');
+  return c.body(null);
+});
+
+app.put('/dog/:id', async (c: Context) => {
+  const id = c.req.param('id');
+  const formData = await c.req.formData();
+  const name = (formData.get('name') as string) || '';
+  const breed = (formData.get('breed') as string) || '';
+  const updatedDog = { id, name, breed };
+  dogs.set(id, updatedDog);
+
+  selectedId = '';
+  c.header('HX-Trigger', 'selection-change');
+  return c.html(dogRow(updatedDog, true));
+});
+
+app.put('/deselect', (c: Context) => {
+  selectedId = '';
+  c.header('HX-Trigger', 'selection-change');
+  return c.body(null);
+});
+
+app.delete('/dog/:id', (c: Context) => {
+  const id = c.req.param('id')
+  dogs.delete(id);
+  return c.body(null);
+});
+
+export default app;
---
+++ event-demo/src/server.ts
@@ -0,0 +1,26 @@
+import {type Context, Hono} from 'hono';
+import {serveStatic} from 'hono/bun';
+
+const app = new Hono();
+
+// Serve static files from the public directory
+app.use('/*', serveStatic({root: './public'}));
+
+app.get('/event-with-no-data', (c: Context) => {
+    c.header('HX-Trigger', 'event1');
+    return c.text('dispatched even1');
+});
+
+app.get('/event-with-string', (c: Context) => {
+    const trigger = {event2: 'some string'};
+    c.header('HX-Trigger', JSON.stringify(trigger));
+    return c.text('dispatched event2');
+});
+
+app.get('/event-with-object', (c: Context) => {
+    const trigger = {event3:  { foo: 1, bar: 2}};
+    c.header('HX-Trigger', JSON.stringify(trigger));
+    return c.text('dispatched event3');
+});
+
+export default app;
